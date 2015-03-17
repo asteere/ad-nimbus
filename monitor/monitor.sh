98bc2891123ce880a115a4f1445712b16bcb46f2
asteere <awsteere@aol.com>
2015-03-17 10:26:20 -0600

    finished really light weight regression tests. Is it time to create a manual regression page? Should we investigate automated testing once we have an elastic demo?

--- monitor/monitor.sh
+++ monitor/monitor.sh
@@ -18,22 +18,42 @@ function selectAndStopAService() {
 
 function checkCpuUsage() {
     host=$1
-    hostCpuAverage=`ssh -oStrictHostKeyChecking=no $host /home/core/share/monitor/calcHostCpuAverage.sh <<%END
-yes
-%END`
+
+    hostCpuAverage=`ssh -oStrictHostKeyChecking=no $host /home/core/share/monitor/calcHostCpuAverage.sh`
 
     echo $host hostCpuAverage=$hostCpuAverage
+}
+
+function startStopServiceBasedOnCpuUsage() {
+    clusterAvgCpuUsage=$1
+    numInCluster=$2
+
     numInstances=`fleetctl list-unit-files | grep $netLocationService | wc -l`
-    if test "$hostCpuAverage" -gt 70 
+
+    if test $numInstances != $numInCluster
+    then
+        echo Error: fleetctl reports $numInstances of $netlocationService. etcdctl reports $numInCluster IP addresses"
+        echo fleetctl:
+        fluf
+
+        echo etcdctl:
+        etctree
+
+        echo Attempt to continue, results may be confusing
+    fi
+ 
+    if test "$hostCpuAverage" -gt 70 -o -f /home/core/share/addService
     then
         nextInstance=$((numInstances+1))
+        echo Start new service
         (cd ~/share/$netLocationService; fleetctl start ${netLocationService}@${nextInstance}.service)
         return
     fi
 
-    if test "$hostCpuAverage" -lt 10 
+    if test "$hostCpuAverage" -lt 10 && $numInstances -gt 2 -o -f /home/core/share/removeService
     then
         # TODO: kill the instance doing the least work
+        echo Shutting down $host
         (cd ~/share/$netLocationService; fleetctl stop ${netLocationService}@${numInstances}.service) 
         return
     fi
@@ -41,29 +61,25 @@ yes
 
 function setup() {
     listOfCoreOs=`etctree 2> /dev/null | grep -e ".*=.*:" | sed '-e s/.*\/\(.*\)=\(.*\)/\1 \2/'`
-    listOfIpAddrs=`echo $listOfCoreOs | sed -e 's/.* //' -e 's/:.*//'`
-    listOfIpAddrsAndPorts=`echo $listOfCoreOs | sed 's/.* //'`
-
-    echo listOfCoreOs
-    echo $listOfCoreOs
-    echo 
-
-    echo listOfIpAddres
-    echo $listOfIpAddres
-    echo 
-
-    echo listOfIpAddresAndPorts
-    echo $listOfIpAddresAndPorts
-    echo 
-
+    listOfIpAddress=`echo $listOfCoreOs | sed -e 's/core-[0-9][0-9] //g' -e 's/:[0-9]*//g'`
+    listOfIpAddressAndPorts=`echo $listOfCoreOs | sed 's/core-[0-9][0-9] //g'`
 }
 
-function forAllNetLocationCoreOs() {
+function checkAllNetLocationCoreOs() {
     setup
-    for i in $listOfIpAddrs
+
+    sumCpuUsage=0
+    numInCluster=0
+    for i in $listOfIpAddress
     do
-        checkCpuUsage $i
+        cpuUsage=`checkCpuUsage $i`
+        sumCpuUsage=`eval $sumCpuUsage + $cpuUsage`
+        numInCluster=`eval $numInCluster + 1`
     done
+
+    clusterAvgCpuUsage=`eval $sumCpuUsage / $numInCluster`
+
+    startStopServiceBasedOnCpuUsage $clusterAvgCpuUsage $numInCluster
 }
 
 function checkResponseTime() {
@@ -74,7 +90,7 @@ if test "$1" == "start"
 then
     while true; 
     do 
-        forAllNetLocationCoreOs
+        checkAllNetLocationCoreOs
         sleep 3 
     done
     exit 0
