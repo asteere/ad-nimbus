14bf350261c77aa52ddfa09ae86848e5d764e99f
asteere <awsteere@aol.com>
2015-04-14 17:54:02 -0600

    4/14/15 EOD: continued work on docker consul

--- consul/startConsul.sh
+++ consul/startConsul.sh
@@ -9,37 +9,59 @@ set +e
 
 hostname=`uname -n`
 
+export GOMAXPROCS=8
 
-# Get number of coreos instances
-numServers=`grep '$num_instances=' config.rb | sed 's/.*=//'`
+# Get number of coreos instances. This works as long as all machines are running etcd ?servers?
+# 
+# numServers=`curl http://127.0.0.1:4001/v2/keys/_etcd/machines | $AD_NIMBUS_DIR/jq '.node.nodes[].value' | wc -l`
+numServers=`etcdctl ls -recursive _etcd/machines | wc -l`
+
+echo etcd reported $numServers servers
+
+# TODO: Override results while in initial development
+numServers=2
 
 # Get number instances of consul running
 # TODO: Start the right number of consul agents and servers based on cluster size
 numInstances=`fleetctl list-unit-files -fields=unit | grep -v UNIT | wc -l`
-numInstances=1
+numInstances=$1
+
+# Not sure why but use the eth0 IP addr
+ETH0_ADDR=${COREOS_PUBLIC_IPV4}
+
+dataCenterArg="-dc superior-dc"
 
 serverArg=-server
+advertiseArg="-advertise=${COREOS_PUBLIC_IPV4}"
+bindArg="-bind=${COREOS_PUBLIC_IPV4}"
+clientArg="-client=${COREOS_PUBLIC_IPV4}"
+
+# TODO: figure out how to get the IP address of the first consul so everybody can join it
+FIXTHIS_IPV4="172.17.8.101"
+joinArg="-join=$FIXTHIS_IPV4"
+
+nodeArg="-node $hostname"
+
+# TODO: Do all the consul servers and agents needs the UI or only some portion
+uiDirArg="-ui-dir ${consulDir}/ui"
+
+dataDirArg="-data-dir /tmp/consul"
+configDirArg="-config-dir ${consulDir}/consul.d"
 
 case "$numInstances" in
 0)
-    # TODO: do we want to always remove all the data. Probably only when we start the cluster the first time
-    rm -rf ~/share/consul/data/*
-    bootstrapArg="-bootstrap-expect $numServers" 
+    # start the first server
 
-    # start the server
-    advertise=${COREOS_PUBLIC_IPV4}
+    bootstrapArg="-bootstrap"
+
+    unset joinArg    
+    
 ;;
-1,2)
-    # start additional servers, no ui
-
-    # TOOD: Figure out the IP addr of the first server
-    join=172.17.8.101
-
-    uiDir="-ui-dir ${consulDir}/ui"
+1|2)
+    # start additional servers
 ;;
 *)
     unset serverArg
-    uiDir="-ui-dir ${consulDir}/ui"
 ;;
 esac
 
@@ -47,24 +69,29 @@ esac
 # If the number instances in less expected servers, start another
 # Otherwise, start agent with UI
 
-#    -p ${COREOS_PRIVATE_IPV4}:8300:8300 \
-#    -p ${COREOS_PRIVATE_IPV4}:8301:8301 \
-#    -p ${COREOS_PRIVATE_IPV4}:8301:8301/udp \
-#    -p ${COREOS_PRIVATE_IPV4}:8302:8302 \
-#    -p ${COREOS_PRIVATE_IPV4}:8302:8302/udp \
-#    -p ${COREOS_PRIVATE_IPV4}:53:53/udp \
+# TODO: do we want to always remove all the data. Probably only when we start the cluster the first time
+rm -rf /tmp/data
+
+set -x
 
 /usr/bin/docker run --name=${consulDockerTag} --rm=true -e "HOST_IP=${COREOS_PUBLIC_IPV4}" \
     --hostname=$hostname \
-    -p ${COREOS_PUBLIC_IPV4}:${consulGuestOSPort}:${consulContainerPort} \
-    -p ${COREOS_PRIVATE_IPV4}:8400:8400 \
-    -p ${COREOS_PRIVATE_IPV4}:8500:8500 \
+    -p ${COREOS_PUBLIC_IPV4}:8300:8300 \
+    -p ${COREOS_PUBLIC_IPV4}:8301:8301 \
+    -p ${COREOS_PUBLIC_IPV4}:8301:8301/udp \
+    -p ${COREOS_PUBLIC_IPV4}:8302:8302 \
+    -p ${COREOS_PUBLIC_IPV4}:8302:8302/udp \
+    -p ${COREOS_PUBLIC_IPV4}:8400:8400 \
+    -p ${COREOS_PUBLIC_IPV4}:8500:8500 \
+    -p ${COREOS_PUBLIC_IPV4}:53:53/udp \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /home/core/share/${consulService}:${consulDir} \
     -v /home/core/share/${nginxService}:${nginxDir} \
     ${consulDockerRegistry}/${consulService}:${consulDockerTag} \
     ${consulDir}/${consulService} \
-    agent $serverArg $uiDir\
-    -config-dir ${consulDir}/consul.d $bootstrapArg\
-    -node $hostname \
-    -data-dir ${consulDir}/data
+    agent $serverArg $bootstrapArg $advertiseArg $joinArg $bindArg $clientArg \
+    $dataCenterArg \
+    $uiDirArg $configDirArg \
+    $dataDirArg \
+    $nodeArg
+
