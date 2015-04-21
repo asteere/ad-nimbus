3735e2fd2efbfe50d8918081fa391c459d9b3bb2
asteere <awsteere@aol.com>
2015-04-20 18:07:49 -0600

    4/20/15 EOD: nginx pulls nginx.conf that confd created from consul and netlocation. Fixed port issues. Hardened the fstart, fstatus and fdestroy functions

--- nginx/startNginx.sh
+++ nginx/startNginx.sh
@@ -1,24 +1,19 @@
 #! /bin/bash
 
-set -e
+set -x 
 
-. /etc/environment
-. /home/core/share/adNimbusEnvironment
-
-set +e
-
-nginxConfFile=/${nginxDir}/nginx.conf
+nginxDir=/opt/nginx
+nginxConfFile=${nginxDir}/nginx.conf
 
 for attempts in {1..30}
 do
-    if test -f ${nginxConfFile}
+    if test -f "${nginxConfFile}"
     then
-        ${nginxDir}/${nginxService} -c ${nginxDir}/nginx.conf
-        return
+        nginx -c "${nginxConfFile}"
+        break
     fi
 
     echo Sleep 10 and see if confd has come up and written the file
     sleep 10
-
 done
 
