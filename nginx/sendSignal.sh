47cfe013db02feac0c9ce172f48dbb7aea94fd5b
asteere <awsteere@aol.com>
2015-05-06 14:02:07 -0600

    make it easier to see that nginx.conf changed

--- nginx/sendSignal.sh
+++ nginx/sendSignal.sh
@@ -11,10 +11,11 @@ function findMachineRunningService() {
 # Find the nginx container id
 if test -d /opt/nginx
 then
-    nginxCidFile=/opt/nginx/nginx.cid
+    nginxDir=/opt/nginx
 else
-    nginxCidFile=/home/core/share/nginx/nginx.cid
+    nginxDir=/home/core/share/nginx/nginx.cid
 fi
+nginxCidFile="$nginxDir"/nginx.cid
 
 if test ! -f "$nginxCidFile"
 then
@@ -26,3 +27,8 @@ nginxId=`cat $nginxCidFile`
 
 # Send the signal to the nginx container id
 echo -e 'POST /containers/'$nginxId'/kill?signal=SIGHUP HTTP/1.0\r\n' | nc -U /var/run/docker.sock
+
+grep 172.17.8 ${nginxDir}/nginx.conf
+echo
+echo
+echo
